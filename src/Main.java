dc9126b93f75080cea8d6ece7697f6a93f5f7550
beloo0 <belinayzehrac@gmail.com>
2026-05-20 19:55:58 +0300

    rastgele sayi

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,12 @@ void main() {
     int yuzlerBasamagi = (int) (Math.floor(Math.random()*9))+1;
     int rastgelesayi =  birlerBasamagi+(onlarBasamagi*10)+(yuzlerBasamagi*100);
     int tahmin =0;
+    int toplamtahmin=0;
     while(true){
         System.out.print("lütfen tahmininizi giriniz..:");
+
         tahmin= tarayici.nextInt();
+        toplamtahmin++;
         if (tahmin>rastgelesayi){
             System.out.println("yanlış bildiniz lütfen daha küçük bir sayı giriniz");
         } else if (tahmin<rastgelesayi) {
@@ -16,12 +19,9 @@ void main() {
 
         }else {
             System.out.println("tebrikler doğru bildiniz");
+            break;
         }
     }
-
-
-
-
-
-
+    int puan= 100-(toplamtahmin-1);
+    System.out.println("tebrikler oyunu tamamladınız puanınız:"+puan);
 }
